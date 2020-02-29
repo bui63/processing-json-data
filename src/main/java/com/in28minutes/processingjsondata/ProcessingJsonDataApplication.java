c1945a8d9cd667aee0108e90df942bad4d677c3d
Rahul Kumar Singh <singhrahulkr1002@gmail.com>
2020-02-29 12:02:13 +0530

    initial commit for processing-json-data template

---
+++ src/main/java/com/in28minutes/processingjsondata/ProcessingJsonDataApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.processingjsondata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingJsonDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingJsonDataApplication.class, args);
+	}
+
+}
